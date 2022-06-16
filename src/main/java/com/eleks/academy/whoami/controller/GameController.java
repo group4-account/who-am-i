2f066f0a0fcefb4d25e370e4b3665984159f8d5b
logicplaner <mariandolynskyi@gmail.com>
2022-06-16 16:54:01 +0300

    [EAG4-19] Quick Game Core implementation (fixed)

--- src/main/java/com/eleks/academy/whoami/controller/GameController.java
+++ src/main/java/com/eleks/academy/whoami/controller/GameController.java
@@ -1,11 +1,9 @@
 package com.eleks.academy.whoami.controller;
 
 import com.eleks.academy.whoami.core.SynchronousPlayer;
-import com.eleks.academy.whoami.core.exception.GameException;
 import com.eleks.academy.whoami.model.request.CharacterSuggestion;
 import com.eleks.academy.whoami.model.request.Message;
 import com.eleks.academy.whoami.model.request.NewGameRequest;
-import com.eleks.academy.whoami.model.request.QuestionAnswer;
 import com.eleks.academy.whoami.model.response.GameDetails;
 import com.eleks.academy.whoami.model.response.GameLight;
 import com.eleks.academy.whoami.model.response.TurnDetails;
@@ -17,11 +15,8 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
-import java.util.Optional;
-import java.util.function.Predicate;
 
 import static com.eleks.academy.whoami.utils.StringUtils.Headers.PLAYER;
-import static org.springframework.web.bind.annotation.RequestMethod.DELETE;
 
 @RestController
 @RequestMapping("/games")
@@ -34,6 +29,9 @@ public class GameController {
 	public List<GameLight> findAvailableGames(@RequestHeader(PLAYER) String player) {
 		return this.gameService.findAvailableGames(player);
 	}
+
+//	@PostMapping
+//	@ResponseStatus(HttpStatus.CREATED)
 	public GameDetails createGame(@RequestHeader(PLAYER) String player,
 								  @Valid @RequestBody NewGameRequest gameRequest) {
 		return this.gameService.createGame(player, gameRequest);
@@ -41,8 +39,10 @@ public class GameController {
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Optional<GameDetails> findQuickGame(@RequestHeader(PLAYER) String player) {
-		return gameService.findAvailableQuickGame(player);
+	public ResponseEntity<GameDetails> findQuickGame(@RequestHeader(PLAYER) String player) {
+		return gameService.findAvailableQuickGame(player)
+				.map(ResponseEntity::ok)
+				.orElseGet(() -> ResponseEntity.notFound().build());
 	}
 
 	@GetMapping("/{id}")
@@ -57,8 +57,7 @@ public class GameController {
 	@PostMapping("/{id}/players")
 	public SynchronousPlayer enrollToGame(@PathVariable("id") String id,
 										  @RequestHeader(PLAYER) String player) {
-		return this.gameService.enrollToGame(id, player).orElseThrow(() -> new GameException("No player"));
-
+		return this.gameService.enrollToGame(id, player);
 	}
 
 	@PostMapping("/{id}/characters")
@@ -66,9 +65,6 @@ public class GameController {
 	public void suggestCharacter(@PathVariable("id") String id,
 								 @RequestHeader(PLAYER) String player,
 								 @Valid @RequestBody CharacterSuggestion suggestion) {
-        Optional.of(player)
-                .filter(string -> string.matches(".{2,50}"))
-				.orElseThrow(() -> new GameException("Player name must be between 2 and 50 characters"));
 		this.gameService.suggestCharacter(id, player, suggestion);
 	}
 
@@ -79,16 +75,7 @@ public class GameController {
 				.map(ResponseEntity::ok)
 				.orElseGet(() -> ResponseEntity.notFound().build());
 	}
-	@GetMapping("/all-players-count")
-	public int getAllPlayersCount() {
-		return this.gameService.getAllPlayersCount();
-	}
 
-	@GetMapping("/{id}/ready-players-count")
-	public int getReadyPlayersCount(@PathVariable("id") String id,
-									@RequestHeader(PLAYER) String player) {
-		return this.gameService.getReadyPlayersCount(id, player);
-	}
 	@PostMapping("/{id}")
 	public ResponseEntity<GameDetails> startGame(@PathVariable("id") String id,
 												 @RequestHeader(PLAYER) String player) {
@@ -99,15 +86,14 @@ public class GameController {
 
 	@PostMapping("/{id}/questions")
 	public void askQuestion(@PathVariable("id") String id,
-							@RequestHeader(PLAYER) String player,
-							@RequestBody Message message) {
+							@RequestHeader(PLAYER) String player, @RequestBody Message message) {
 		this.gameService.askQuestion(id, player, message.getMessage());
 	}
 
 	@PostMapping("/{id}/guess")
 	public void submitGuess(@PathVariable("id") String id,
 							@RequestHeader(PLAYER) String player, @RequestBody Message message) {
-		this.gameService.submitGuess(id, player, QuestionAnswer.valueOf(message.getMessage()));
+		this.gameService.submitGuess(id, player, message.getMessage());
 	}
 
 	@PostMapping("/{id}/answer")
@@ -116,9 +102,5 @@ public class GameController {
 		this.gameService.answerQuestion(id, player, message.getMessage());
 
 	}
-	@DeleteMapping("/{id}/leave")
-	public void leaveGame(@PathVariable("id") String id,
-						  @RequestHeader(PLAYER) String player) {
-		this.gameService.leaveGame(id, player);
-	}
+
 }
