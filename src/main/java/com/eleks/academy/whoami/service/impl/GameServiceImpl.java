cbbbfcabc2e10c09a1d65b2ee3ce655cef1486f1
Valerii Sierhieiev <vsierhieiev-its24@lll.kpi.ua>
2022-07-13 11:47:54 +0300

    fix NPE in leaveGame

--- src/main/java/com/eleks/academy/whoami/service/impl/GameServiceImpl.java
+++ src/main/java/com/eleks/academy/whoami/service/impl/GameServiceImpl.java
@@ -5,14 +5,12 @@ import com.eleks.academy.whoami.core.SynchronousPlayer;
 import com.eleks.academy.whoami.core.exception.GameException;
 import com.eleks.academy.whoami.core.impl.Answer;
 import com.eleks.academy.whoami.core.impl.PersistentGame;
+import com.eleks.academy.whoami.core.impl.PersistentPlayer;
 import com.eleks.academy.whoami.core.state.GameState;
 import com.eleks.academy.whoami.model.request.CharacterSuggestion;
 import com.eleks.academy.whoami.model.request.NewGameRequest;
 import com.eleks.academy.whoami.model.request.QuestionAnswer;
-import com.eleks.academy.whoami.model.response.GameDetails;
-import com.eleks.academy.whoami.model.response.GameLight;
-import com.eleks.academy.whoami.model.response.PlayerState;
-import com.eleks.academy.whoami.model.response.TurnDetails;
+import com.eleks.academy.whoami.model.response.*;
 import com.eleks.academy.whoami.repository.AddAnswerRequest;
 import com.eleks.academy.whoami.repository.AddQuestionRequest;
 import com.eleks.academy.whoami.repository.GameRepository;
@@ -135,7 +133,7 @@ public class GameServiceImpl implements GameService {
 
 		this.gameRepository.findById(gameId).stream()
 				.findFirst()
-				.map(sg -> sg.getPlayersInGame())
+				.map(SynchronousGame::getPlayersInGame)
 				.ifPresent(pwsl -> this.qnaHistoryRepository.AddQuestionRequest(new AddQuestionRequest(false, gameId, player, message), pwsl));
 	}
 
@@ -170,16 +168,20 @@ public class GameServiceImpl implements GameService {
 	}
 
 	@Override
-	public void leaveGame(String gameId, String playerId)
-	{
+	public void leaveGame(String gameId, String playerId) {
 		SynchronousGame game = this.gameRepository.findById(gameId)
+				.filter(game1 -> game1.getPlayersInGame()
+						.stream()
+						.map(PlayerWithState::getPlayer)
+						.map(PersistentPlayer::getId)
+						.anyMatch(id -> id.equals(playerId)))
 				.orElseThrow(
 						() -> new GameException(String.format("ROOM_NOT_FOUND_BY_ID", gameId)));
 		game.removeFromGame(gameId, playerId);
 		if (Optional.ofNullable(game.getPlayersInGame())
-			.map(List::size)
-			.map(size -> size.equals(0))
-			.orElse(false)){
+				.map(List::size)
+				.map(size -> size.equals(0))
+				.orElse(false)) {
 			this.gameRepository.remove(game);
 		}
 
