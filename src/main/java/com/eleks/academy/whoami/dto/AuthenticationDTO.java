398527bd17f7a5e5aaed25c8d71931772370fb88
Valerii Sierhieiev <vsierhieiev-its24@lll.kpi.ua>
2022-07-29 17:27:32 +0300

    HEROKU

--- src/main/java/com/eleks/academy/whoami/dto/AuthenticationDTO.java
+++ src/main/java/com/eleks/academy/whoami/dto/AuthenticationDTO.java
@@ -1,11 +1,15 @@
 package com.eleks.academy.whoami.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.RequiredArgsConstructor;
 
 @Data
+@RequiredArgsConstructor
+@AllArgsConstructor
 public class AuthenticationDTO  {
 
-	private String email;
+		private String email;
 
 	private String password;
 
